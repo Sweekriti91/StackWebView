090502f9cfeb30296af4fb7ccb6b0d729b14f880
Sweekriti Satpathy <swsat@microsoft.com>
2022-08-23 15:55:53 -0700

    code

---
+++ StackWebView/StackWebView/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace StackWebView;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ StackWebView/StackWebView/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StackWebView;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
